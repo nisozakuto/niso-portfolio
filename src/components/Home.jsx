f4cdf979bed6ce395711bac1f3092bbda96e94e4
nisozakuto <nisozakuto@gmail.com>
2020-09-20 20:41:11 -0400

    updated text

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -28,10 +28,8 @@ export default class Projects extends Component {
                             <h3>Looking for a solution? <span className="span-color"> I am a great problem solver</span></h3>
                         </article>
                         <aside>
-                            <p> What is Lorem Ipsum?
-                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-                            Test
-                          </p>
+                            <p>I am currently working on my <code><em>React JS</em></code>, <code><em>Node JS</em></code> and <code><em>Ruby on Rails</em></code>.</p>
+                            <p> </p>
                         </aside>
                     </section>
                     :
