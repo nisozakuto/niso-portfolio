302c912e61b8e618b301cf1e0311b770cfc87ec3
nisozakuto <nisozakuto@gmail.com>
2020-11-12 12:10:14 -0500

    Broke down the pieces into components

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,13 +1,7 @@
 import React, { Component } from 'react'
-import { FaGithub } from "react-icons/fa"
-import { FaLinkedin } from "react-icons/fa"
-import { FaRegEnvelope } from "react-icons/fa"
-import blackjack from '../assets/blackjack.png'
-import photobook from '../assets/photobook.png'
-import msnmtdb from '../assets/msnmtdb.png'
-import coffeeapp from '../assets/coffeeapp.png'
-
-export default class Projects extends Component {
+import Projects from './Projects'
+import Contact from './Contact'
+export default class Home extends Component {
     constructor(props) {
         super(props)
         this.state = {
@@ -44,80 +38,14 @@ export default class Projects extends Component {
                         </section>
                         :
                         (this.props.currentPage === 'Projects') ?
-                            <section className="projects-section">
-                                <h1>Projects</h1>
-                                <ul className="projects-ul">
-                                    <div className="project-box">
-                                        <img src={blackjack} width="500px" alt="A card game made with HTML, CSS and JS only" />
-                                        <h3>Blackjack game</h3>
-                                        <article>
-                                            A card game built with Javascript only. Some of the instructions as follows :
-                                            <ul>
-                                                <li>- First you need to let the game know that you are starting the game</li>
-                                                <li>- Each game, initally, player puts $10 in order to play. Player has to bet before the cards are dealt. It is not possible to bet once you hit a card</li>
-                                                <li>- Which takes us to Hit / Stand stage:</li>
-                                                <li>- Hit will give you another random card from the deck.</li>
-                                                <li>- Will change the turn to the dealer.</li>
-                                                <li>- Dealer has to hit as long as, they have less than 16 in number values.</li>
-                                            </ul>
-                                        </article>
-                                        <p>Github page: <a href="https://github.com/nisozakuto/Blackjack" target="_blank" rel="noreferrer">github.com/nisozakuto/Blackjack</a></p>
-                                        <p>Visit page: <a href="https://nisozakuto.github.io/Blackjack/" target="_blank" rel="noreferrer">Blackjack</a></p>
-                                    </div>
-                                    <div className="project-box">
-                                        <img src={photobook} width="500px" alt="Photobook project made with Express JS" />
-                                        <h3>Photobook App</h3>
-                                        <article>
-                                            Photobook app is created to help photographers to post their albums and share them with their clients.
-                                            Currently, the app is letting visitors to sign up, login, create albums based on the keywords that are available and browse between pictures.
-                                        </article>
-                                        <p>Github page: <a href="https://github.com/nisozakuto/ThePhotobook" target="_blank" rel="noreferrer">github.com/nisozakuto/ThePhotobook</a></p>
-                                        <p>Visit page: <a href="https://thephotobook.herokuapp.com/" target="_blank" rel="noreferrer">The Photobook</a></p>
-
-                                    </div>
-                                    <div className="project-box">
-                                        <img src={msnmtdb} width="500px" alt="Team work project made with Node JS, Express and React JS" />
-                                        <h3>Movie TV Database App</h3>
-                                        <p>Github page: <a href="https://github.com/nisozakuto/MSN-MTDB" target="_blank" rel="noreferrer">github.com/nisozakuto/MSN-MTDB</a></p>
-                                        <p>Visit page: <a href="https://evening-basin-19807.herokuapp.com/" target="_blank" rel="noreferrer">Staging Page</a></p>
-
-                                    </div>
-                                    <div className="project-box">
-                                        <img src={coffeeapp} width="500px" alt="Coffee app project with Ruby on Rails and React" />
-                                        <h3>Reactive Coffee</h3>
-                                        <p>Github page: <a href="https://github.com/nisozakuto/ReactiveCoffee" target="_blank" rel="noreferrer">github.com/nisozakuto/ReactiveCoffee</a></p>
-                                        <p>Visit page: <a href="https://reactive-coffee-staging.herokuapp.com/" target="_blank" rel="noreferrer">Reactive Coffee</a></p>
-
-                                    </div>
-                                </ul>
-                            </section>
+                            <Projects />
                             :
                             (this.props.currentPage === 'Contact') ?
-                                <section className="home-page-section">
-                                    <h1 className="center">Contact</h1>
-                                    <section className="contact">
-                                        <article>
-                                            <h3>Choose one of the options to get in touch </h3>
-                                            <ul className="contact-ul">
-                                                <h3><FaLinkedin /> <a target="_blank" href="https://www.linkedin.com/in/nisozakuto">LinkedIn</a></h3>
-                                                <h3><FaRegEnvelope /> <a target="_blank" href="mailto:nisozakuto@gmail.com">Email</a></h3>
-                                            </ul>
-                                        </article>
-                                        <article>
-                                            <h3>Check out my Github profile</h3>
-                                            <ul>
-                                                <h3><FaGithub /><a target="_blank" href="https://github.com/nisozakuto">Github</a> </h3>
-                                            </ul>
-                                        </article>
-                                    </section>
-                                </section>
+                                <Contact />
                                 :
-                                <div>na</div>
+                                <div>Wrong Path</div>
                 }
             </main>
         )
     }
 }
-
-
-// Deleted the yarn.lock
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,25 +1,69 @@
 import React, { Component } from 'react'
-
+import blackjack from '../assets/blackjack.jpg'
+import photobook from '../assets/photobook.jpg'
+import msnmtdb from '../assets/msnmtdb.jpg'
+import coffeeapp from '../assets/coffeeapp.jpg'
+import passwordGenerator from '../assets/passwordGenerator.jpg'
 export default class Projects extends Component {
     render() {
         return (
-            <div>
-                <section>
-                    <h2>Projects:</h2>
-                    <ul>
-                        <h3>Blackjack</h3>
-                        <h3>Photobook</h3>
-                        <h3>Movie - Series Database</h3>
-                        <h3>Project IV</h3>
-                        <h3>Portfolio Project</h3>
-                    </ul>
-                </section>
-                <section>
-                    <p>explain the project here
-                    Changed for main
-                    </p>
-                </section>
-            </div>
+            <section className="projects-section">
+                <h1>Projects</h1>
+                <ul className="projects-ul">
+                    <div className="project-box">
+                        <img src={blackjack} width="500px" alt="A card game made with HTML, CSS and JS only" />
+                        <h3>Blackjack game</h3>
+                        <article>
+                            A card game built with Javascript only. Some of the instructions as follows :
+                        <ul>
+                                <li>- First you need to let the game know that you are starting the game</li>
+                                <li>- Each game, initally, player puts $10 in order to play. Player has to bet before the cards are dealt. It is not possible to bet once you hit a card</li>
+                                <li>- Which takes us to Hit / Stand stage:</li>
+                                <li>- Hit will give you another random card from the deck.</li>
+                                <li>- Will change the turn to the dealer.</li>
+                                <li>- Dealer has to hit as long as, they have less than 16 in number values.</li>
+                            </ul>
+                        </article>
+                        <p>Github page: <a href="https://github.com/nisozakuto/Blackjack" target="_blank" rel="noreferrer">github.com/nisozakuto/Blackjack</a></p>
+                        <p>Visit page: <a href="https://nisozakuto.github.io/Blackjack/" target="_blank" rel="noreferrer">Blackjack</a></p>
+                    </div>
+
+                    <div className="project-box">
+                        <img src={photobook} width="500px" alt="Photobook project made with Express JS" />
+                        <h3>Photobook App</h3>
+                        <article>
+                            Photobook app is created to help photographers to post their albums and share them with their clients.
+                            Currently, the app is letting visitors to sign up, login, create albums based on the keywords that are available and browse between pictures.
+                    </article>
+                        <p>Github page: <a href="https://github.com/nisozakuto/ThePhotobook" target="_blank" rel="noreferrer">github.com/nisozakuto/ThePhotobook</a></p>
+                        <p>Visit page: <a href="https://thephotobook.herokuapp.com/" target="_blank" rel="noreferrer">The Photobook</a></p>
+                    </div>
+
+                    <div className="project-box">
+                        <img src={msnmtdb} width="500px" alt="Team work project made with Node JS, Express and React JS" />
+                        <h3>Movie TV Database App</h3>
+                        <p>Github page: <a href="https://github.com/nisozakuto/MSN-MTDB" target="_blank" rel="noreferrer">github.com/nisozakuto/MSN-MTDB</a></p>
+                        <p>Visit page: <a href="https://evening-basin-19807.herokuapp.com/" target="_blank" rel="noreferrer">Staging Page</a></p>
+                    </div>
+
+                    <div className="project-box">
+                        <img src={coffeeapp} width="500px" alt="Coffee app project with Ruby on Rails and React" />
+                        <h3>Reactive Coffee</h3>
+                        <p>Github page: <a href="https://github.com/nisozakuto/ReactiveCoffee" target="_blank" rel="noreferrer">github.com/nisozakuto/ReactiveCoffee</a></p>
+                        <p>Visit page: <a href="https://reactive-coffee-staging.herokuapp.com/" target="_blank" rel="noreferrer">Reactive Coffee</a></p>
+                    </div>
+
+                    <div className="project-box">
+                        <img src={passwordGenerator} width="500px" alt="Photobook project made with Express JS" />
+                        <h3>Password Generator</h3>
+                        <article>
+                            This app was created to serve individuals who need a strong password. The app gives you four different options to create the password.
+                    </article>
+                        <p>Github page: <a href="https://github.com/nisozakuto/password-generator" target="_blank" rel="noreferrer">github.com/nisozakuto/password-generator</a></p>
+                    </div>
+                </ul>
+            </section>
+
         )
     }
 }
