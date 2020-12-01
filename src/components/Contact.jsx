450a670f6316df2c07ae1fdbafafc65171e7aac1
nisozakuto <nisozakuto@gmail.com>
2020-12-01 13:06:28 -0500

    added rel="noreferrer"

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -11,14 +11,14 @@ export default class Contact extends Component {
                     <article>
                         <h3>Choose one of the options to get in touch </h3>
                         <ul className="contact-ul">
-                            <h3><FaLinkedin /> <a target="_blank" href="https://www.linkedin.com/in/nisozakuto">LinkedIn</a></h3>
-                            <h3><FaRegEnvelope /> <a target="_blank" href="mailto:nisozakuto@gmail.com">Email</a></h3>
+                            <h3><FaLinkedin /> <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/nisozakuto">LinkedIn</a></h3>
+                            <h3><FaRegEnvelope /> <a target="_blank" rel="noreferrer" href="mailto:nisozakuto@gmail.com">Email</a></h3>
                         </ul>
                     </article>
                     <article>
                         <h3>Check out my Github profile</h3>
                         <ul>
-                            <h3><FaGithub /><a target="_blank" href="https://github.com/nisozakuto">Github</a> </h3>
+                            <h3><FaGithub /><a target="_blank" rel="noreferrer" href="https://github.com/nisozakuto">Github</a> </h3>
                         </ul>
                     </article>
                 </section>
