6ba7cf80854245d1b5a634ffdde914653d864e99
nisozakuto <nisozakuto@gmail.com>
2020-12-06 11:29:08 -0500

    created a new component

---
+++ src/components/Project.jsx
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+
+export default class Project extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+        }
+    }
+    componentDidMount() {
+    }
+    render() {
+        return (
+            <>
+                <div className="project-box">
+                    <img src={this.props.imageSrc} width="500px" alt={this.props.alt} />
+                    <h3>{this.props.title}</h3>
+                    <article>
+                        {this.props.text}
+                    </article>
+                    <p>Github page: <a href={this.props.githubLink} target="_blank" rel="noreferrer">{this.props.githubLinkA}</a></p>
+                    <p>Visit page: <a href={this.props.visitPage} target="_blank" rel="noreferrer">{this.props.text}</a></p>
+                </div>
+            </>
+        )
+    }
+}
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,70 +1,66 @@
-import React, { Component } from 'react'
-import blackjack from '../assets/blackjack.jpg'
-import photobook from '../assets/photobook.jpg'
-import msnmtdb from '../assets/msnmtdb.jpg'
-import coffeeapp from '../assets/coffeeapp.jpg'
-import passwordGenerator from '../assets/passwordGenerator.jpg'
+import React, { Component } from "react";
+import blackjack from "../assets/blackjack.jpg";
+import photobook from "../assets/photobook.jpg";
+import msnmtdb from "../assets/msnmtdb.jpg";
+import coffeeapp from "../assets/coffeeapp.jpg";
+import passwordGenerator from "../assets/passwordGenerator.jpg";
+import Project from "../components/Project";
 export default class Projects extends Component {
-    render() {
-        return (
-            <section className="projects-section">
-                <h1>Projects</h1>
-                <ul className="projects-ul">
-                    <div className="project-box">
-                        <img src={blackjack} width="500px" alt="A card game made with HTML, CSS and JS only" />
-                        <h3>Blackjack game</h3>
-                        <article>
-                            A card game built with Javascript only. Some of the instructions as follows :
-                        <ul>
-                                <li>- First you need to let the game know that you are starting the game</li>
-                                <li>- Each game, initally, player puts $10 in order to play. Player has to bet before the cards are dealt. It is not possible to bet once you hit a card</li>
-                                <li>- Which takes us to Hit / Stand stage:</li>
-                                <li>- Hit will give you another random card from the deck.</li>
-                                <li>- Will change the turn to the dealer.</li>
-                                <li>- Dealer has to hit as long as, they have less than 16 in number values.</li>
-                            </ul>
-                        </article>
-                        <p>Github page: <a href="https://github.com/nisozakuto/Blackjack" target="_blank" rel="noreferrer">github.com/nisozakuto/Blackjack</a></p>
-                        <p>Visit page: <a href="https://nisozakuto.github.io/Blackjack/" target="_blank" rel="noreferrer">Blackjack</a></p>
-                    </div>
-
-                    <div className="project-box">
-                        <img src={photobook} width="500px" alt="Photobook project made with Express JS" />
-                        <h3>Photobook App</h3>
-                        <article>
-                            Photobook app is created to help photographers to post their albums and share them with their clients.
-                            Currently, the app is letting visitors to sign up, login, create albums based on the keywords that are available and browse between pictures.
-                    </article>
-                        <p>Github page: <a href="https://github.com/nisozakuto/ThePhotobook" target="_blank" rel="noreferrer">github.com/nisozakuto/ThePhotobook</a></p>
-                        <p>Visit page: <a href="https://thephotobook.herokuapp.com/" target="_blank" rel="noreferrer">The Photobook</a></p>
-                    </div>
-
-                    <div className="project-box">
-                        <img src={msnmtdb} width="500px" alt="Team work project made with Node JS, Express and React JS" />
-                        <h3>Movie TV Database App</h3>
-                        <p>Github page: <a href="https://github.com/nisozakuto/MSN-MTDB" target="_blank" rel="noreferrer">github.com/nisozakuto/MSN-MTDB</a></p>
-                        <p>Visit page: <a href="https://evening-basin-19807.herokuapp.com/" target="_blank" rel="noreferrer">Staging Page</a></p>
-                    </div>
-
-                    <div className="project-box">
-                        <img src={coffeeapp} width="500px" alt="Coffee app project with Ruby on Rails and React" />
-                        <h3>Reactive Coffee</h3>
-                        <p>Github page: <a href="https://github.com/nisozakuto/ReactiveCoffee" target="_blank" rel="noreferrer">github.com/nisozakuto/ReactiveCoffee</a></p>
-                        <p>Visit page: <a href="https://reactive-coffee-staging.herokuapp.com/" target="_blank" rel="noreferrer">Reactive Coffee</a></p>
-                    </div>
-
-                    <div className="project-box">
-                        <img src={passwordGenerator} width="500px" alt="Photobook project made with Express JS" />
-                        <h3>Password Generator</h3>
-                        <article>
-                            This app was created to serve individuals who need a strong password. The app gives you four different options to create the password.
-                    </article>
-                        <p>Github page: <a href="https://github.com/nisozakuto/password-generator" target="_blank" rel="noreferrer">github.com/nisozakuto/password-generator</a></p>
-                        <p>Visit Netlify page: <a href="https://z-password-generator.netlify.app" target="_blank" rel="noreferrer">Password Generator</a></p>
-                    </div>
-                </ul>
-            </section>
-
-        )
-    }
+  render() {
+    return (
+      <section className="projects-section">
+        <h1>Projects</h1>
+        <ul className="projects-ul">
+          <Project
+            imageSrc={blackjack}
+            alt="A card game made with HTML, CSS and JS only"
+            title="Blackjack game"
+            text="A card game built with Javascript only"
+            githubLink="https://github.com/nisozakuto/Blackjack"
+            githubLinkA="github.com/nisozakuto/Blackjack"
+            visitPage="https://nisozakuto.github.io/Blackjack/"
+          />
+          <Project
+            imageSrc={photobook}
+            alt="Photobook project made with Express JS"
+            title="Photobook App"
+            text=" Photobook app is created to help photographers to post their
+            albums and share them with their clients. Currently, the app is
+            letting visitors to sign up, login, create albums based on the
+            keywords that are available and browse between pictures."
+            githubLink="https://github.com/nisozakuto/ThePhotobook"
+            githubLinkA="github.com/nisozakuto/ThePhotobook"
+            visitPage="https://thephotobook.herokuapp.com"
+          />
+          <Project
+            imageSrc={msnmtdb}
+            alt="Team work project made with Node JS, Express and React JS"
+            title="Movie TV Database App"
+            text="This is app enables you to browse through Movies and TV shows in an easy way"
+            githubLink="https://github.com/nisozakuto/MSN-MTDB"
+            githubLinkA="github.com/nisozakuto/MSN-MTDB"
+            visitPage="https://evening-basin-19807.herokuapp.com"
+          />
+          <Project
+            imageSrc={coffeeapp}
+            alt="Coffee app project with Ruby on Rails and React"
+            title="Reactive Coffee"
+            text="Ready to order your coffe, virtually?"
+            githubLink="https://github.com/nisozakuto/ReactiveCoffee"
+            githubLinkA="github.com/nisozakuto/ReactiveCoffee"
+            visitPage="https://reactive-coffee-staging.herokuapp.com"
+          />
+          <Project
+            imageSrc={passwordGenerator}
+            alt="Password generator app that has crypto password generation option"
+            title="Password Generator"
+            text="This app was created to serve individuals who need a strong password. The app gives you four different options to create the password."
+            githubLink="https://github.com/nisozakuto/password-generator"
+            githubLinkA="github.com/nisozakuto/password-generator"
+            visitPage="https://z-password-generator.netlify.app"
+          />
+        </ul>
+      </section>
+    );
+  }
 }
