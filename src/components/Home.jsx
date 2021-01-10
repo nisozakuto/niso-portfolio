48fe80a19c72fe1df98671e8de168b38dd254d34
nisozakuto <nisozakuto@gmail.com>
2021-01-10 14:59:49 -0500

    removed obselete tab

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,13 +8,7 @@ const element = <FontAwesomeIcon icon={faCode} />;
 export default class Home extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      // Blackjack_info: 'Blackjack game built with Javascript',
-      // Photobook_info: "Photobook app, connecting clients with the Photographers. Client's photographer will send you the pictures through this application.Client will be able to go into the brand new album, browse through the old albums, like, delete the photos that they like.",
-      // Movie_info: 'This was my first team work project using Node JS and React JS. I had a lot fun working in a 3 people team and dividing different tasks. Coming over from the complications together gives me an idea what working in a team and building project would look like.',
-      // ProjectIV_info: 'My 4th project is a Coffee Shop application, Ruby on Rails in the backend and React in the front end.',
-      // Portfolio_info: 'You are viewing right now :)'
-    };
+    this.state = {};
   }
   leaveHome = () => {};
   render() {
@@ -40,11 +34,8 @@ export default class Home extends Component {
               >
                 Learn more
               </button>
-              <FontAwesomeIcon icon="coffee" border />
             </article>
           </section>
-        ) : this.props.currentPage === "About" ? (
-          <section></section>
         ) : this.props.currentPage === "Projects" ? (
           <Projects />
         ) : this.props.currentPage === "Contact" ? (
